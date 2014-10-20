8c11111a691c71238866c4889447f6c08907574e
tomasro27 <tomasro27@yahoo.com>
2014-10-20 13:50:56 -0500

    hacktx version

--- fitnessbird/Assets/Scripts/PipeSpawner.cs
+++ fitnessbird/Assets/Scripts/PipeSpawner.cs
@@ -7,16 +7,17 @@ public class PipeSpawner : MonoBehaviour {
 	public float distanceOfPipeGap = 4;
 	public float maxPipeHeightChange = 2;
 	public GameObject pipePrefab;
+	public GameObject triggerPrefab;
 	public Transform player;
 	public int numOfPipePairs = 100;
-	private float prevHeight = 3;
+	private float prevHeight = 5;
 	private float prevDist;
 	private ArrayList pipes;
 
 	// Use this for initialization
 	void Start () {
 		pipes = new ArrayList ();
-		prevDist = distanceBetweenPipeSets;
+		prevDist = distanceBetweenPipeSets *1.5f;
 		for (int i = 0; i < numOfPipePairs; i++) {
 						spawnPipePair (prevDist);
 			prevDist += distanceBetweenPipeSets;
@@ -30,23 +31,27 @@ public class PipeSpawner : MonoBehaviour {
 
 	void spawnPipePair (float zLocation) {
 		prevHeight -= 3;
-				prevHeight = Random.Range (prevHeight - maxPipeHeightChange, prevHeight + maxPipeHeightChange);
-				prevHeight = Mathf.Clamp (prevHeight, 0, 15);
+		prevHeight = Random.Range (prevHeight - maxPipeHeightChange, prevHeight + maxPipeHeightChange);
+		prevHeight = Mathf.Clamp (prevHeight, 0, 15);
 		prevHeight += 3;
 		GameObject pipe = (GameObject) Instantiate (pipePrefab
-		                              			   , new Vector3 (0
-		                                            			 , prevHeight - (distanceOfPipeGap / 2)
-		             											 , zLocation)
-		                              			   , player.rotation);
+		                                            , new Vector3 (0
+		               , prevHeight - (distanceOfPipeGap / 2)
+		               , zLocation)
+		                                            , player.rotation);
 		pipe.transform.GetComponent<PipeScript> ().ResizePipe(true);
 		pipes.Add(pipe);
 		pipe = (GameObject) Instantiate (pipePrefab
-		                                , new Vector3 (0
-		               								  , prevHeight + (distanceOfPipeGap / 2)
-		               								  , zLocation)
-		                                , player.rotation);
+		                                 , new Vector3 (0
+		               , prevHeight + (distanceOfPipeGap / 2)
+		               , zLocation)
+		                                 , player.rotation);
 		pipe.transform.GetComponent<PipeScript> ().ResizePipe(false);
 		pipes.Add(pipe);
+		pipe = (GameObject)Instantiate (triggerPrefab
+		                                 , new Vector3 (0, prevHeight, zLocation)
+		                                 , player.rotation);
+		pipe.transform.localScale = new Vector3 (1, distanceOfPipeGap, 1);
 		}
 
 }
--- fitnessbird/Assets/Scripts/PlayerMovement.cs
+++ fitnessbird/Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
+
+using Parse;
 
 public class PlayerMovement : MonoBehaviour {
 
@@ -9,10 +13,18 @@ public class PlayerMovement : MonoBehaviour {
 	private bool downswing = true;
 	public float jumpSpeed = 5f;
 	public bool dead = false;
+	public int score = 0;
+	public TextMesh scoreDisplay;
 
 	// Use this for initialization
 	void Start () {
-	
+		ParseUser.LogInAsync ("HACKTX", "hacktx").ContinueWith (t =>
+				{
+						if (t.IsFaulted || t.IsCanceled) {
+						} else {
+								// Login was successful.
+						}
+				});
 	}
 	
 	// Update is called once per frame
@@ -24,22 +36,24 @@ public class PlayerMovement : MonoBehaviour {
 								upswing = true;
 								downswing = false;
 								Jump ();
-								print ("upswing" + (transform.GetComponentsInChildren<ThalmicMyo> () [0].accelerometer.magnitude - 1));
+								//print ("upswing" + (transform.GetComponentsInChildren<ThalmicMyo> () [0].accelerometer.magnitude - 1));
 						} else if (upswing && transform.GetComponentsInChildren<ThalmicMyo> () [0].accelerometer.magnitude - 1 < -.25f) {
 								upswing = false;
 								crest = true;
-								print ("crest" + (transform.GetComponentsInChildren<ThalmicMyo> () [0].accelerometer.magnitude - 1));
+								//print ("crest" + (transform.GetComponentsInChildren<ThalmicMyo> () [0].accelerometer.magnitude - 1));
 						
 						} else if (crest && transform.GetComponentsInChildren<ThalmicMyo> () [0].accelerometer.magnitude - 1 > .25f) {
 								crest = false;
 								downswing = true;
-								print ("downswing" + (transform.GetComponentsInChildren<ThalmicMyo> () [0].accelerometer.magnitude - 1));
+								//print ("downswing" + (transform.GetComponentsInChildren<ThalmicMyo> () [0].accelerometer.magnitude - 1));
 						}
 				} else {
-			if(transform.GetComponentsInChildren<ThalmicMyo> () [0].pose == Thalmic.Myo.Pose.Fist) {
-				print ("FIST!");
-			}
-			}
+						if (transform.GetComponentsInChildren<ThalmicMyo> () [0].pose == Thalmic.Myo.Pose.Fist) {
+								if (transform.GetComponentsInChildren<ThalmicMyo> () [0].accelerometer.magnitude - 1 > .5f) {
+										Application.LoadLevel (Application.loadedLevel);
+								}
+						}
+				}
 			}
 			
 	void Jump() {
@@ -52,5 +66,21 @@ public class PlayerMovement : MonoBehaviour {
 		for (int i = 0; i < childList.Length; i++) {
 			childList [i].enabled = true;
 		}
+		//	var query = ParseObject.GetQuery("GameScore")
+		//		.WhereEqualTo("playerName", "Dan Stemkoski");
+		//query.FindAsync().ContinueWith(t =>
+		 //                              {
+		//	ParseObject results = t.Result;
+		//});
+		IList list = ParseUser.CurrentUser.Get<List<object>> ("scores");
+		list.Add (score);
+		ParseUser.CurrentUser.Remove ("scores");
+		ParseUser.CurrentUser.Add ("scores", list);
+		ParseUser.CurrentUser.SaveAsync ();
 	}
-}
+
+	void OnTriggerEnter(Collider other) {
+		score++;
+		scoreDisplay.text = score.ToString ();
+		}
+	}
